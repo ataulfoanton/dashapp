d610b110f14e8b273bc525a626592b5a20956086
Fitzcarrald <50465862+Fitzcarrald@users.noreply.github.com>
2022-02-20 20:36:13 +0100

    Added general performance section

--- callbacks.py
+++ callbacks.py
@@ -10,6 +10,13 @@ import pickle
 
 from utils.functions import *
 
+# Import training set
+X_train = reduce_memory_usage(
+    pd.read_csv(
+        'data/X_train.csv',
+        index_col='SK_ID_CURR')
+)
+X_train.replace({0: 'Non-defaulter', 1: 'Defaulter'}, inplace=True)
 # Import testing sets
 X_test = reduce_memory_usage(
     pd.read_csv(
@@ -31,6 +38,50 @@ feature_coef = pd.Series(
     log_reg.named_steps.logisticregression.coef_[0],
     index=log_reg.named_steps.columntransformer.get_feature_names_out()
 ).rename('coefficient')
+
+# Relevant features
+client_info_cols = [
+    'CODE_GENDER',
+    'FLAG_OWN_CAR',
+    'AMT_INCOME_TOTAL',
+    'NAME_TYPE_SUITE',
+    'NAME_INCOME_TYPE',
+    'NAME_EDUCATION_TYPE',
+    'NAME_FAMILY_STATUS',
+    'NAME_HOUSING_TYPE',
+    'AGE_YEARS',
+    'YEARS_EMPLOYED',
+    'OCCUPATION_TYPE',
+    'ORGANIZATION_TYPE'
+]
+loan_info_cols = [
+    'NAME_CONTRACT_TYPE',
+    'AMT_CREDIT',
+    'AMT_ANNUITY',
+    'AMT_GOODS_PRICE'
+]
+elements_info_cols = [
+    'FLAG_DOCUMENT_2',
+    'FLAG_DOCUMENT_3',
+    'FLAG_DOCUMENT_4',
+    'FLAG_DOCUMENT_5',
+    'FLAG_DOCUMENT_6',
+    'FLAG_DOCUMENT_7',
+    'FLAG_DOCUMENT_8',
+    'FLAG_DOCUMENT_9',
+    'FLAG_DOCUMENT_10',
+    'FLAG_DOCUMENT_11',
+    'FLAG_DOCUMENT_12',
+    'FLAG_DOCUMENT_13',
+    'FLAG_DOCUMENT_14',
+    'FLAG_DOCUMENT_15',
+    'FLAG_DOCUMENT_16',
+    'FLAG_DOCUMENT_17',
+    'FLAG_DOCUMENT_18',
+    'FLAG_DOCUMENT_19',
+    'FLAG_DOCUMENT_20',
+    'FLAG_DOCUMENT_21'
+]
 
 
 # # Callbacks
@@ -79,49 +130,6 @@ def update_loan_score(ID):
     ]
 )
 def update_loan_details(ID, val_button):
-    client_info_cols = [
-        'CODE_GENDER',
-        'FLAG_OWN_CAR',
-        'AMT_INCOME_TOTAL',
-        'NAME_TYPE_SUITE',
-        'NAME_INCOME_TYPE',
-        'NAME_EDUCATION_TYPE',
-        'NAME_FAMILY_STATUS',
-        'NAME_HOUSING_TYPE',
-        'AGE_YEARS',
-        'YEARS_EMPLOYED',
-        'OCCUPATION_TYPE',
-        'ORGANIZATION_TYPE'
-    ]
-    loan_info_cols = [
-        'NAME_CONTRACT_TYPE',
-        'AMT_CREDIT',
-        'AMT_ANNUITY',
-        'AMT_GOODS_PRICE'
-    ]
-    elements_info_cols = [
-        'FLAG_DOCUMENT_2',
-        'FLAG_DOCUMENT_3',
-        'FLAG_DOCUMENT_4',
-        'FLAG_DOCUMENT_5',
-        'FLAG_DOCUMENT_6',
-        'FLAG_DOCUMENT_7',
-        'FLAG_DOCUMENT_8',
-        'FLAG_DOCUMENT_9',
-        'FLAG_DOCUMENT_10',
-        'FLAG_DOCUMENT_11',
-        'FLAG_DOCUMENT_12',
-        'FLAG_DOCUMENT_13',
-        'FLAG_DOCUMENT_14',
-        'FLAG_DOCUMENT_15',
-        'FLAG_DOCUMENT_16',
-        'FLAG_DOCUMENT_17',
-        'FLAG_DOCUMENT_18',
-        'FLAG_DOCUMENT_19',
-        'FLAG_DOCUMENT_20',
-        'FLAG_DOCUMENT_21'
-    ]
-
     if ID:
         if val_button == 'information':
             cols = client_info_cols
@@ -136,3 +144,13 @@ def update_loan_details(ID, val_button):
         file = []
 
     return file
+
+# General performance callback
+@app.callback(
+    Output('general-performance', 'figure'),
+    [Input('feature', 'value')]
+)
+def update_general_performance(feat):
+    fig = train_density_with_central_tendencies_by_target(feat, X_train)
+    
+    return fig
